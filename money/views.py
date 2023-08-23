6bd07476e1d28e7961870aaf948f1d9591ad1d91
Giyos1 <giyosoripov4@gmail.com>
2023-08-23 14:01:17 +0500

    ...

--- money/views.py
+++ money/views.py
@@ -174,7 +174,7 @@ class AutoPayViewSet(ModelViewSet):
             paid_amount = data['paid_amount']
             deadline = data['deadline']
             description = data['description']
-            if paid_amount != instance.paid_amount and wallet == instance.wallet:
+            if paid_amount != instance.paid_amount:
                 m1 = MoneyItem.objects.create(
                     wallet=wallet,
                     money=money,
